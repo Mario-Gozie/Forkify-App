291f39501c71838be188e74167d2e6c620630921
Mario-Gozie <oguedoihucg@gmail.com>
2024-12-28 23:50:24 +0200

    setting up things

---
+++ src/js/views/paginationView.js
@@ -0,0 +1,8 @@
+import view from "./view";
+import icons from "../../img/icons.svg";
+
+class paginationView extends view {
+  _parentElement = document.querySelector(".pagination");
+
+  _generateMarkup() {}
+}
